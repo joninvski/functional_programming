7b673c121eebf06836ac0b3330c6315ab9edee1c
Joao Trindade <trindade.joao@gmail.com>
2013-10-10 20:32:00 +0100

    New fetch for older weeks

---
+++ funsets/fetch_source.sh
@@ -0,0 +1,2 @@
+wget "http://spark-public.s3.amazonaws.com/progfun/assignments/funsets.zip" -O funsets.zip 
+unzip -n funsets.zip -d ..
---
+++ recfun/fetch_source.sh
@@ -0,0 +1,2 @@
+wget "http://spark-public.s3.amazonaws.com/progfun/assignments/recfun.zip" -O recfun.zip 
+unzip -n recfun.zip -d ..
